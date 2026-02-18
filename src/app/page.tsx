30604cc14c4722ba0d04cfb751bb1a035b3001fa
matheusVBP <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>
2026-02-17 22:57:07 -0300

    adicionando o card, botao do input e input! estilizando um pouquinho e colocando um main tambem.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-//import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Ola Mundo</h1>
-    </div>
+    <main className="w-full h-screen bg-base-300 flex justify-center items-center">
+      <Card className="w-lg">
+        <CardContent className="flex gap-2">
+          <Input />
+          <Button>ola</Button>
+        </CardContent>
+      </Card>
+    </main>
   );
 }
